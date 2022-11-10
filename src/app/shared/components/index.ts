81c93d481382f2852f3c98404a58a18a9a7bdcbc
Jader Moura <jader_vinicius_gonzaga_moura@epam.com>
2022-11-10 09:37:34 +0100

    implemented array of components in shared modules

---
+++ src/app/shared/components/index.ts
@@ -0,0 +1,15 @@
+import { HeaderComponent } from './header/header.component';
+import { ButtonComponent } from './button/button.component';
+import { CardComponent } from './course-card/course-card.component';
+import { InfoComponent } from './info/info.component';
+import { ModalComponent } from './modal/modal.component';
+import { FormComponent } from './form/form.component';
+
+export const Components = [
+  HeaderComponent,
+  ButtonComponent,
+  CardComponent,
+  InfoComponent,
+  ModalComponent,
+  FormComponent,
+];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,29 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
-import { ButtonComponent } from './components/button/button.component';
-import { CardComponent } from './components/course-card/course-card.component';
-import { InfoComponent } from './components/info/info.component';
-import { ModalComponent } from './components/modal/modal.component';
-import { FormComponent } from './components/form/form.component';
+import { Components } from './components';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    ButtonComponent,
-    CardComponent,
-    InfoComponent,
-    ModalComponent,
-    FormComponent,
-  ],
+  declarations: [Components],
   imports: [CommonModule],
-  exports: [
-    HeaderComponent,
-    ButtonComponent,
-    CardComponent,
-    InfoComponent,
-    ModalComponent,
-    FormComponent,
-  ],
+  exports: [Components],
 })
 export class SharedModule {}
