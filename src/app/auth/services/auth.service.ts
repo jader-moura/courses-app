f63e7acef32a0f4ec50a01cc43e1cc3ec9f0b70c
Jader Moura <jader_vinicius_gonzaga_moura@epam.com>
2022-11-24 23:21:07 +0100

    router navigate changed to window location

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -35,7 +35,8 @@ export class AuthService {
     this.httpClient.post('http://localhost:4000/login', loginPayload).subscribe(
       ({ result }: any) => {
         this.sessionStorage.setToken(result);
-        this.router.navigate(['/courses']);
+        window.location.href = '/courses';
+        // this.router.navigate(['/courses']);
       },
       (err: HttpErrorResponse) => console.error(`Got error: ${err}`)
     );
@@ -52,12 +53,14 @@ export class AuthService {
       .subscribe(
         () => {
           this.sessionStorage.deleteToken();
-          this.router.navigate(['/login']);
+          window.location.href = '/login';
+          // this.router.navigate(['/login']);
         },
         ({ status }: HttpErrorResponse) => {
           if (status === 401) {
             this.sessionStorage.deleteToken();
-            this.router.navigate(['/login']);
+            window.location.href = '/login';
+            // this.router.navigate(['/login']);
           }
         }
       );
@@ -71,7 +74,9 @@ export class AuthService {
           if (successful) {
             alert('Account created with success, please login');
           }
-          this.router.navigate(['/login']);
+          window.location.href = '/login';
+
+          // this.router.navigate(['/login']);
         },
         (err: HttpErrorResponse) => console.error(`Got error: ${err}`)
       );
